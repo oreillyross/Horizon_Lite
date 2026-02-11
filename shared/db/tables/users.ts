de6c29825bb6c2f9995c65244c36bb212110342b
Faktor 10 <oreillyross@gmail.com>
2026-02-11 10:19:43 +0000

    add

--- shared/db/tables/users.ts
+++ shared/db/tables/users.ts
@@ -4,9 +4,7 @@ import { pgTable, text, varchar, uuid } from "drizzle-orm/pg-core";
 export const users = pgTable("users", {
   id: varchar("id").primaryKey().default(sql`gen_random_uuid()`),
 
-
   username: text("username").unique(),
-
 
   email: text("email").notNull().unique(),
 
